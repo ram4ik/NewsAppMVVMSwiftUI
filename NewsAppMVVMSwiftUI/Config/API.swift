ac81acfd7c5c36589cde0be6c9a0af33471d257d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-29 15:46:38 +0300

    project init

---
+++ NewsAppMVVMSwiftUI/Config/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  NewsAppMVVMSwiftUI
+//
+//  Created by ramil on 29.09.2020.
+//
+
+import Foundation
+
+struct API {
+    static let key = ""
+}
---
+++ NewsAppMVVMSwiftUI/Models/News.swift
@@ -0,0 +1,22 @@
+//
+//  News.swift
+//  NewsAppMVVMSwiftUI
+//
+//  Created by ramil on 29.09.2020.
+//
+
+import Foundation
+
+struct News: Decodable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let urlToImage: String?
+    let url: String?
+}
+
+struct NewsEnvelope: Decodable {
+    let status: String
+    let totalResults: Int
+    let articles: [News]
+}
