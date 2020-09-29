16f970e6aa7be28736b1d2db30ceb27518fc3338
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-29 16:01:30 +0300

    creating Network Layer

---
+++ NewsAppMVVMSwiftUI/Services/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  NewsAppMVVMSwiftUI
+//
+//  Created by ramil on 29.09.2020.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    private let baseUrlString = "https://newsapi.org/v2"
+    private let usTopHeadline = "top-headlines?country=us"
+    
+    func getNews(completion: @escaping (([News]?) -> Void)) {
+        let urlString = "\(baseUrlString)\(usTopHeadline)&apikey=\(API.key)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil, let data = data else {
+                completion(nil)
+                return
+            }
+            
+            let newsRes = try? JSONDecoder().decode(NewsEnvelope.self, from: data)
+            newsRes == nil ? completion(nil) : completion(newsRes!.articles)
+        }.resume()
+    }
+}
