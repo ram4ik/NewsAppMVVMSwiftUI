ada14b9b76ea7aeaff398d8fd538eca48c674e74
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-29 15:15:53 +0300

    Initial Commit

---
+++ NewsAppMVVMSwiftUI/NewsAppMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppMVVMSwiftUIApp.swift
+//  NewsAppMVVMSwiftUI
+//
+//  Created by ramil on 29.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
